25d899e93eafbd3206d53a7e989a5dc87c6a4a23
Ömercan Göktaş <77095758+omercngoktas@users.noreply.github.com>
2021-01-17 00:23:55 +0300

    CSE 1142 - Project 4

---
+++ project4/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student
+{
+    char *name;
+    char *surname;
+    long int ID;
+};
+
+int main()
+{
+    struct Student *currentStudent;
+    FILE *students;
+    students = fopen()
+
+
+
+
+
+    return 0;
+}
