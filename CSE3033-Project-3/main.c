a376ece457b594de7107066d97bf8fa1bcca93e0
Ömercan Göktaş <77095758+omercngoktas@users.noreply.github.com>
2023-01-05 12:45:53 +0300

    updates

--- CSE3033-Project-3/main.c
+++ CSE3033-Project-3/main.c
@@ -30,6 +30,14 @@ lines* textLines;
 
 int sizeOfLines = 0;
 
+void unlockAllLines() {
+    for(int i = 0; i < sizeOfLines; i++) {
+        textLines[i].isLineReplaced = 0;
+        textLines[i].isLineUppered = 0;
+        printf("Line %d has unlocked\n", i);
+    }
+}
+
 void addNewLine(char newLine[], int lineNumber) {
     if(sizeOfLines == 0) {
         textLines = (lines*) malloc(lineNumber * sizeof(lines));
@@ -81,6 +89,48 @@ void* read_thread(void* threadType) {
     pthread_exit(NULL);
 }
 
+void* upper_thread(void* threadType) {
+    thread_data* upper_thread_data = (thread_data*) threadType;
+    int thread_id = upper_thread_data->thread_id;
+
+    int readLineNumber = 0;
+    for(int i = 0; i < sizeOfLines; i++) {
+        if(readLineNumber % NUMBER_OF_UPPER_THREADS == thread_id) {
+            pthread_mutex_lock(&mutex);    
+            printf("Upper thread_id: %d\tLine which is gonna be uppered is: %d\n", thread_id, readLineNumber);
+            printf("Line: %s\n", textLines[readLineNumber].lineString);
+            for(int j = 0; j < LINE_LENGTH; j++) {
+                textLines[readLineNumber].lineString[j] = toupper(textLines[readLineNumber].lineString[j]);
+            }
+            printf("Line: %s\n\n", textLines[readLineNumber].lineString);
+            pthread_mutex_unlock(&mutex);
+        }
+        readLineNumber++;
+    }
+    
+    pthread_exit(NULL);
+}
+
+void* replace_thread(void* threadType) {
+    thread_data* data = (thread_data*) threadType;
+    int thread_id = data->thread_id;
+
+    int readLineNumber = 0;
+    for(int i = 0; i < sizeOfLines; i++) {
+        if(readLineNumber % NUMBER_OF_REPLACE_THREADS == thread_id) {
+            pthread_mutex_lock(&mutex);
+            printf("Replace thread_id: %d\tLine which is gonna be replaced is: %d\n", thread_id, readLineNumber);
+            printf("Line: %s\n", textLines[readLineNumber].lineString);
+            for(int j = 0; j < LINE_LENGTH; j++) {
+                if(textLines[readLineNumber].lineString[j] == ' ') { textLines[readLineNumber].lineString[j] = '_'; }
+            }
+            printf("Line: %s\n\n", textLines[readLineNumber].lineString);
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+
+}
+
 void printLines() {
     for(int i = 0; i < sizeOfLines; i++) {
         textLines[i].isLineReplaced = 0;
@@ -91,8 +141,8 @@ void printLines() {
 }
 
 int main(int argc, char* argv[]) {
-    if(argc != 3) {
-        printf("Usage: %s <fileName> readThreadNumber\n", argv[0]);
+    if(argc != 5) {
+        printf("Usage: %s <fileName> readThreadNumber upperThreadNumber replaceThreadNumber\n", argv[0]);
         exit(1);
     }
 
@@ -100,14 +150,23 @@ int main(int argc, char* argv[]) {
 
     fileName = argv[1];
     NUMBER_OF_READ_THREADS = atoi(argv[2]);
+    NUMBER_OF_UPPER_THREADS = atoi(argv[3]);
+    NUMBER_OF_REPLACE_THREADS = atoi(argv[4]);
 
     pthread_t readThreads[NUMBER_OF_READ_THREADS];
     thread_data readThreadsData[NUMBER_OF_READ_THREADS];
+
+    pthread_t upperThreads[NUMBER_OF_UPPER_THREADS];
+    thread_data upperThreadsData[NUMBER_OF_UPPER_THREADS];
+
+    pthread_t replaceThreads[NUMBER_OF_REPLACE_THREADS];
+    thread_data replaceThreadsData[NUMBER_OF_REPLACE_THREADS];
+
     for(int i = 0; i < NUMBER_OF_READ_THREADS; i++) {
         readThreadsData[i].thread_id = i;
         int isReadThreadSuccessful = pthread_create(&readThreads[i], NULL, read_thread, &readThreadsData[i]);
         if(isReadThreadSuccessful != 0) {
-            perror("Error creating read thread");
+            perror("Error while creating read thread");
             exit(1);
         }
     }
@@ -115,10 +174,48 @@ int main(int argc, char* argv[]) {
     for(int i = 0; i < NUMBER_OF_READ_THREADS; i++) {
         int isReadThreadTerminated = pthread_join(readThreads[i], NULL);
         if(isReadThreadTerminated != 0) {
-            perror("Error waiting read thread");
+            perror("Error while waiting read thread");
             exit(1);
         }
     }
+
+    unlockAllLines();
+
+    for(int i = 0; i < NUMBER_OF_UPPER_THREADS; i++) {
+        upperThreadsData[i].thread_id = i;
+        int isUpperThreadSuccessful = pthread_create(&upperThreads[i], NULL, upper_thread, &upperThreadsData[i]);
+        if(isUpperThreadSuccessful != 0) {
+            perror("Error while creating upper thread");
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < NUMBER_OF_REPLACE_THREADS; i++) {
+        replaceThreadsData[i].thread_id = i;
+        int isReplaceThreadSuccessful = pthread_create(&replaceThreads[i], NULL, replace_thread, &replaceThreadsData[i]);
+        if(isReplaceThreadSuccessful != 0) {
+            perror("Error while creating replace thread");
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < NUMBER_OF_REPLACE_THREADS; i++) {
+        int isReplaceThreadTerminated = pthread_join(replaceThreads[i], NULL);
+        if(isReplaceThreadTerminated != 0) {
+            perror("Error while waiting replace thread");
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < NUMBER_OF_UPPER_THREADS; i++) {
+        int isUpperThreadTerminated = pthread_join(upperThreads[i], NULL);
+        if(isUpperThreadTerminated != 0) {
+            perror("Error while waiting upper thread");
+            exit(1);
+        }
+    }
+
+
 
     printLines();
 
